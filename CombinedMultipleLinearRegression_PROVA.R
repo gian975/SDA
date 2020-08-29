f998a41bab867c6993d38919d9625c1638873454
gian975 <G.DAMORE14@studenti.unisa.it>
2020-08-29 18:13:53 +0200

    Test CombinedLinearRegression

---
+++ CombinedMultipleLinearRegression_PROVA.R
@@ -0,0 +1,40 @@
+setwd("~/GitHub/SDA")
+data_complete <- read.csv("dataset/data_complete.csv", header=TRUE)
+
+head(data_complete)
+names(data_complete)
+
+install.packages('caTools')
+library(caTools)
+my_data <- data_complete[,c(3,7,8,9,10,11,12,13,14,15,16)]
+split = sample.split(my_data$co2_emission, SplitRatio = 0.8)
+tr_s = subset(my_data, split == TRUE)
+t_s = subset(my_data, split == FALSE)
+
+model <- lm(co2_emission ~ year + euro_standard + transmission_type +
+              fuel_type + urban_metric + extra_urban_metric + combined_metric + noise_level + fuel_cost_6000_miles, data = tr_s)
+
+library(corrplot)
+res <- cor(my_data, use="pairwise.complete.obs")
+round(res, 2)
+dev.new()
+plot.new()
+dev.off()
+corrplot(res, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+
+combined_model_1 <- lm(co2_emission ~ year + euro_standard + transmission_type +
+                       fuel_type + urban_metric*fuel_cost_6000_miles*extra_urban_metric*combined_metric + noise_level, data = tr_s)
+
+combined_model_2<- lm(co2_emission ~ year + euro_standard + transmission_type +
+                       fuel_type + fuel_cost_6000_miles*combined_metric + noise_level, data = tr_s)
+
+combined_model_3<- lm(co2_emission ~ year + transmission_type +
+                        fuel_type + fuel_cost_6000_miles*combined_metric + noise_level, data = tr_s)
+
+plot(combined_model_1)
+plot(combined_model_2)
+plot(combined_model_3)
+
+
+
