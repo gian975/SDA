423f79403a1bff933db042c0605f4e9c9c7b4b56
Giuliap199 <58252210+Giuliap199@users.noreply.github.com>
2020-08-30 17:50:37 +0200

    resampling e ridge/lasso

---
+++ resampling.R
@@ -0,0 +1,230 @@
+
+attach(t_s)
+attach(tr_s)
+
+fit.linear <- (co2_emission ~ euro_standard + fuel_cost_6000_miles + fuel_type + engine_capacity + year + transmission_type
+               + noise_level + combined_metric + urban_metric + extra_urban_metric)
+
+fit.reduced <- (co2_emission ~ year + euro_standard + transmission_type + engine_capacity +
+                                     fuel_type + fuel_cost_6000_miles + noise_level)
+
+fit.poly2 <- (co2_emission ~ poly(year,2) + poly(euro_standard,2) + transmission_type + poly(engine_capacity,2) +
+                 fuel_type + poly(fuel_cost_6000_miles,2) + poly(noise_level,2))
+
+fit.log <- (co2_emission ~ log(year) + log(euro_standard) + transmission_type + log(engine_capacity) +
+                fuel_type + log(fuel_cost_6000_miles) + log(noise_level))
+
+n = nrow(tr_s)
+c=nrow(t_s)
+
+
+######### Validation Set Approch #########
+train=sample(1:n,n)
+test=sample(1:c,c)
+set.seed(1)
+lm.fit=lm(fit.linear, data = data_complete , subset = train)
+
+# the estimated test MSE for the linear regression fit is 33.51284 (seed=1)
+y_true=data_complete$co2_emission
+y_predict=predict(lm.fit,data_complete)
+mean(((y_true-y_predict)[test])^2)
+
+# the estimated test MSE for the linear regression reduced fit is 114.237
+lm.fit2=lm(fit.reduced , data = data_complete, subset=train)
+
+y_true=data_complete$co2_emission
+y_predict=predict(lm.fit2,data_complete)
+mean(((y_true-y_predict)[test])^2)
+
+# use the poly() function to estimate the test error for the polynomials-2 transformation.
+lm.fit3=lm(fit.poly2, data = data_complete, subset=train) 
+
+# the estimated test MSE for the linear regression fit is 67.6406
+y_true=data_complete$co2_emission
+y_predict=predict(lm.fit3,data_complete)
+mean(((y_true-y_predict)[test])^2)
+
+
+# estimate the test error for the log transformation. 251.937
+lm.fit4=lm(fit.log, data = data_complete, subset=train) 
+
+y_true=data_complete$co2_emission
+y_predict=predict(lm.fit4,data_complete)
+mean(((y_true-y_predict)[test])^2)
+
+
+########## K-Fold Cross Validation ##########
+library(boot)
+
+glm.fit=glm(fit.linear ,data=data_complete)
+
+cv.err=cv.glm(data_complete,glm.fit, K = 10)
+cv.err$delta # The K-Fold Cross validation estimate for the test error is approximately  48.12944 (seed=1).
+
+# K-Fold Cross validation for polynomial regressions with orders i=1,2,...,4.
+
+cv.error=rep(0,4)
+for (i in 1:4){
+  glm.fit=glm(fit.poly2, data = data_complete)
+  cv.error[i]=cv.glm(data_complete,glm.fit, K=10)$delta[1]
+}
+cv.error
+## we obtain a test error higher -> 74.30688 73.93336 74.07724 74.46966
+
+
+########## Bootstrap ##########
+
+# The boot.fn() function can also be used in order to create bootstrap estimates 
+# for the intercept and slope terms by randomly sampling from among the observations with replacement
+# We will compare the estimates obtained using the bootstrap to those obtained using the previous models
+library(stringr)
+indice=nrow(data_complete)
+
+# No-transformation
+set.seed (2)
+boot.fn=function(data,index){
+  return(coef(lm(co2_emission ~ euro_standard + fuel_cost_6000_miles + fuel_type + engine_capacity + year + transmission_type
+                  + noise_level + combined_metric + urban_metric + extra_urban_metric, data = data,subset=index)))
+}
+boot.fn(data_complete, 1:indice)
+
+# Boot estimate is not deterministic
+boot.fn(data_complete,sample(1:n, 45441,replace=T))
+boot.fn(data_complete,sample(1:n, 45441,replace=T))
+# We use the boot() function to compute the standard errors 
+# of 1,000 bootstrap estimates for the intercept and slope terms.
+b = boot(data_complete ,boot.fn ,1000)
+
+s = summary(lm(fit.linear, data = data_complete))
+
+# Take all std. errors of the bootstrap estimate 
+x <- capture.output(b)
+x <- str_extract(x, "^t[0-9.]+.*$")
+x <- x[!is.na(x)]
+se <- as.numeric(unlist(str_extract_all(x, '[0-9.]+$')))
+
+# Take all std. errors of the linear model
+c = s$coefficients[ ,2]
+c = as.numeric(c)
+
+cat("\nDifference between no-Transformation Std.errors:\n",c - se,"\n")
+
+
+# Polinomials-2 no-linear transformation
+set.seed (2)
+
+boot.fn=function(data,index){
+  return(coef(lm(co2_emission ~ poly(year,2) + poly(euro_standard,2) + transmission_type + poly(engine_capacity,2) +
+                   fuel_type + poly(fuel_cost_6000_miles,2) + poly(noise_level,2),data = data,subset=index)))
+}
+boot.fn(data_complete, 1:indice)
+
+boot.fn(data_complete,sample(1:indice, 45441,replace=T))
+
+# We use the boot() function to compute the standard errors 
+# of 1,000 bootstrap estimates for the intercept and slope terms.
+
+b = boot(data_complete ,boot.fn ,1000)
+
+s = summary(lm(fit.poly2,data = data_complete))
+
+# Take all std. errors of the bootstrap estimate 
+x <- capture.output(b)
+x <- str_extract(x, "^t[0-9.]+.*$")
+x <- x[!is.na(x)]
+se <- as.numeric(unlist(str_extract_all(x, '[0-9.]+$')))
+
+# Take all std. errors of the poly-2 transformation
+c = s$coefficients[ ,2]
+c = as.numeric(c)
+
+cat("\nDifference between poly-2 transformation Std.errors:\n",c - se,"\n")
+
+# Polinomials-3 no-linear transformation
+set.seed (2)
+
+boot.fn=function(data,index){
+  temp <- index
+  return(coef(lm(fit.poly3, data = data, subset=temp)))
+}
+
+boot.fn(merComplete, 1:n)
+
+boot.fn(merComplete,sample(1:n, 79576,replace=T))
+
+b = boot(merComplete ,boot.fn ,1000)
+
+s = summary(lm(fit.poly3, data = merComplete))
+
+# Take all std. errors of the bootstrap estimate 
+x <- capture.output(b)
+x <- str_extract(x, "^t[0-9.]+.*$")
+x <- x[!is.na(x)]
+se <- as.numeric(unlist(str_extract_all(x, '[0-9.]+$')))
+
+# Take all std. errors of the poly-3 transformation
+c = s$coefficients[ ,2]
+c = as.numeric(c)
+
+cat("\nDifference between poly-3 transformation Std.errors:\n",c - se,"\n")
+
+# Polinomials-4 no-linear transformation
+set.seed (2)
+
+boot.fn=function(data,index){
+  return(coef(lm(fit.poly4, data = data,subset=index)))
+}
+
+boot.fn(merComplete, 1:n)
+
+
+boot.fn(merComplete,sample(1:n, 79576,replace=T))
+
+boot(merComplete ,boot.fn ,1000)
+
+summary(lm(fit.poly4, data = merComplete))
+
+# Take all std. errors of the bootstrap estimate 
+x <- capture.output(b)
+x <- str_extract(x, "^t[0-9.]+.*$")
+x <- x[!is.na(x)]
+se <- as.numeric(unlist(str_extract_all(x, '[0-9.]+$')))
+
+# Take all std. errors of the poly-4 transformation
+c = s$coefficients[ ,2]
+c = as.numeric(c)
+
+cat("\nDifference between poly-4 transformation Std.errors:\n",c - se,"\n")
+
+
+######## Plot ########
+
+## Plot linear model
+dev.new()
+plot(as.factor(seat_comfort),overall,main='Linear Model', xlab="seat comfort", ylab="overall")
+xx=seq(min(seat_comfort),max(seat_comfort),along.with = seat_comfort)
+ci_lin <- predict(lm(overall~seat_comfort,data=merComplete),newdata=data.frame(seat_comfort=xx),se.fit = T,interval = "confidence")
+matplot(xx,ci_lin$fit[,1],lty=1, ltw=2, col="red", type="l", add=T)
+
+## Plot linear model with polinomials-2 transformation
+dev.new()
+plot(as.factor(seat_comfort),overall,main='Polinomial-2 Model', xlab="seat comfort", ylab="overall")
+xx=seq(min(seat_comfort),max(seat_comfort),along.with = seat_comfort)
+ci_lin <- predict(lm(overall~I(seat_comfort^2),data=merComplete),newdata=data.frame(seat_comfort=xx),se.fit = T,interval = "confidence")
+matplot(xx,ci_lin$fit[,1],lty=1, ltw=2, col="red", type="l", add=T)
+
+## Plot linear model with polinomials-3 transformation
+dev.new()
+plot(as.factor(seat_comfort),overall,main='Polinomial-3 Model', xlab="seat comfort", ylab="overall")
+xx=seq(min(seat_comfort),max(seat_comfort),along.with = seat_comfort)
+ci_lin <- predict(lm(overall~I(seat_comfort^3),data=merComplete),newdata=data.frame(seat_comfort=xx),se.fit = T,interval = "confidence")
+matplot(xx,ci_lin$fit[,1],lty=1, ltw=2, col="red", type="l", add=T)
+
+## Plot linear model with polinomials-4 transformation
+dev.new()
+plot(as.factor(seat_comfort),overall,main='Polinomial-4 Model', xlab="seat comfort", ylab="overall")
+xx=seq(min(seat_comfort),max(seat_comfort),along.with = seat_comfort)
+ci_lin <- predict(lm(overall~I(seat_comfort^4),data=merComplete),newdata=data.frame(seat_comfort=xx),se.fit = T,interval = "confidence")
+matplot(xx,ci_lin$fit[,1],lty=1, ltw=2, col="red", type="l", add=T)
+
+
---
+++ ridge_lasso.R
@@ -0,0 +1,149 @@
+library(glmnet)
+attach(tr_s)
+
+fit.linear <- (co2_emission ~ euro_standard + fuel_cost_6000_miles + fuel_type + engine_capacity + year + transmission_type
+               + noise_level + combined_metric + urban_metric + extra_urban_metric)
+
+
+# The model.matrix() function is particularly useful for creating x; not only does it produce a matrix 
+# corresponding to the 9 predictors but it also automatically transforms any qualitative
+# variables into dummy variables (recommended).
+# The latter property is important because glmnet() can only take numerical, quantitative inputs.
+
+x = model.matrix(fit.linear, data_complete)[,-1] #[-1] means no intercept
+
+y = data_complete$co2_emission
+
+
+#=================================
+#  RIDGE
+#=================================
+
+#VALORI ARBITRARI DI LAMBDA
+
+grid=10^seq(10,-2,length=100) # Lambda values grid (from 10^10 to 10^-2)
+
+ridge.mod=glmnet(x,y,alpha=0,lambda=grid)
+
+#Associated with each value of lambda is a vector of ridge regression 
+#coefficients, stored in a matrix that can be accessed by coef(), 
+#in this case 12x100, 11+intercept for each lambda value:
+dim(coef(ridge.mod))
+
+
+#50-ESIMO VALOREDI LAMBDA
+
+# We expect the coefficient estimates to be much smaller, in terms of l2 norm, 
+#when a large value of lambda is used, as compared to when a small value is used. 
+ridge.mod$lambda[50] # grid[50] = 11497.57
+
+coef(ridge.mod)[,50] # corresponding coefficients
+
+sqrt(sum(coef(ridge.mod)[-1,50]^2)) # l2 norm = 0.33 (DISTANZA DA 0)
+
+#60 ESIMO VALORE DI LAMBDA
+ridge.mod$lambda[60] # lambda = 705.48
+
+coef(ridge.mod)[,60] # corresponding coefficients
+sqrt(sum(coef(ridge.mod)[-1,60]^2)) # l2 norm = 3.82> l2 for lambda[50]
+
+# obtain the ridge regression coefficients for a new lambda, say 50:
+predict(ridge.mod,s=50,type="coefficients")[1:11,] 
+
+
+# Validation approach to estimate test error
+set.seed(1)
+train=sample(1:nrow(x), nrow(x)/2) # another typical approach to sample
+test=(-train)
+y.test=y[test]
+# fit a ridge regression model on the training set, and evaluate its MSE on the test set, using lambda = 4. 
+ridge.mod=glmnet(x[train,],y[train],alpha=0,lambda=grid,thresh=1e-12)
+ridge.pred=predict(ridge.mod,s=4,newx=x[test,]) # Note the use of the predict() function again. This time we get predictions for a test set, by replacing type="coefficients" with the newx argument.
+mean((ridge.pred-y.test)^2) # test MSE = 58,59
+mean((mean(y[train ])-y.test)^2) # test MSE, if we had instead simply fit a model with just an intercept, we would have predicted each test observation using the mean of the training observations.
+
+#ALL AUMENTARE DI LAMBDA I COEEFICIENTI DIMUISCONO E MSE AUMENTA , IN PARTICOLARE
+# IL TEST MSE DELLA RIDGE CON LAMBDA = 10^10 CORRISPONDE AL TEST MSE ADDATTANDO
+#IL MODELLO CON SOLO L'INTERCETTA 
+
+
+# PREDICTION WITH LAMBDA= 10^10 , CORRESPONDING TO MODEL WITH ONLY INTERCEPT
+ridge.pred=predict(ridge.mod,s=1e10,newx=x[test,])
+mean((ridge.pred-y.test)^2) #  3206.242
+
+
+# PREDICTION WITH LAMBDA= 0, CORRESPONDING TO LEAST SQUARE
+ridge.pred=predict(ridge.mod,s=0,newx=x[test,],exact=T,x=x[train,],y=y[train]) # corrected according to errata (glmnet pack updated)
+mean((ridge.pred-y.test)^2) #69,41
+
+## CROSS VALIDATION TO CHOOSE THE BEST LAMBDA 
+
+set.seed (1)
+cv.out=cv.glmnet(x[train,],y[train],alpha=0)
+dev.new()
+plot(cv.out)
+bestlam=cv.out$lambda.min; 
+bestlam; #5.618761
+log(bestlam) # 1.726111
+cv.out$lambda.1se
+ridge.pred=predict(ridge.mod,s=bestlam ,newx=x[test,])
+mean((ridge.pred-y.test)^2) #64,23  #IMPROVEMENT OF MSE RESPECT LAMBDA=4
+
+
+#Finally refit our ridge regression model on the full data set with the best lambda
+
+out=glmnet(x,y,alpha=0)
+predict(out,type="coefficients",s=bestlam)[1:11,]
+# As expected, none of the coefficients are zero
+# ridge regression does not perform variable selection!
+dev.new()
+plot.new()
+plot(out,label = T, xvar = "lambda")
+legend("bottomright", lwd = 1, col = 1:6, legend = colnames(x), cex = .5)
+
+
+
+#=================================
+#  LASSO
+#=================================
+
+# use the argument alpha = 1 to perform lasso
+
+lasso.mod = glmnet(x[train,], y[train], alpha=1, lambda=grid)
+dev.new()
+plot(lasso.mod,label = T)
+dev.new()
+plot.new()
+plot(lasso.mod,label = T, xvar = "lambda")
+# perform cross-validation
+set.seed (1)
+cv.out=cv.glmnet(x[train,],y[train],alpha=1)
+dev.new()
+plot(cv.out)
+bestlam=cv.out$lambda.min
+print(bestlam) #0.01563454
+print(log(bestlam)) #-4.158273
+print(cv.out$lambda.1se)
+lasso.pred=predict(lasso.mod,s=bestlam ,newx=x[test,]) 
+mean((lasso.pred-y.test)^2)  #56,15
+
+lasso.pred=predict(lasso.mod,s=0,newx=x[test,],exact=T,x=x[train,],y=y[train])
+mean((lasso.pred-y.test)^2)
+# However, the lasso has a substantial advantage:
+# 1 of the 11 coefficient estimates is exactly zero (extra_urban_metric) .
+out=glmnet(x,y,alpha=1,lambda=grid)
+lasso.coef=predict(out,type="coefficients",s=bestlam)[1:11,]
+lasso.coef
+lasso.coef[lasso.coef!=0]
+cat("Number of coefficients equal to 0:",sum(lasso.coef==0),"\n")
+
+# compare with OLS when only selected predictors are included. 
+
+fit.lm=lm(fit.linear <- (co2_emission ~ euro_standard + fuel_cost_6000_miles + fuel_type + engine_capacity + year + transmission_type
+                         + noise_level + combined_metric + urban_metric ), data=tr_s)
+coef(fit.lm)
+lasso.coef=predict(out,type="coefficients",s=0)[1:11,]
+lasso.coef
+# coef(lm(Salary~., data=Hitters)) # differences on the 3rd place
+
+
